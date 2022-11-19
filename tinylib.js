2104725592d5824d4b629d3e50a9695ad5f485a0
Magnogen <hydronesium@gmail.com>
2022-11-19 14:37:44 +0000

    Little tinylib updates

--- tinylib.js
+++ tinylib.js
@@ -1,8 +1,14 @@
 // jshint esversion: 11
 const $ = query => document.querySelector(query);
 const $$ = query => document.querySelectorAll(query);
-EventTarget.prototype.on = function (...args) { return this.addEventListener(...args) };
-EventTarget.prototype.trigger = function (name, options={}) { return this.dispatchEvent(Object.assign(new Event(name), options)) };
+EventTarget.prototype.on = function (...args) {
+    return this.addEventListener(...args);
+};
+EventTarget.prototype.trigger = function (name, options={}) {
+    let event = new Event(name);
+    for (const key in options) event[key] = options[key];
+    return this.dispatchEvent(event)
+};
 
 const min = (a, b) => a < b ? a : b;
 const max = (a, b) => a > b ? a : b;
