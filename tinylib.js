a7bd98528d600692afc9c2f75aa9a02d29e32724
Magnogen <hydronesium@gmail.com>
2022-11-24 17:35:25 +0000

    add hash and update freya credit tinylibjs

--- tinylib.js
+++ tinylib.js
@@ -13,7 +13,7 @@ EventTarget.prototype.trigger = function (name, options={}) {
 const abs = (v)    => v < 0 ? -v : v;
 const max = (a, b) => a < b ? b : a;
 const min = (a, b) => a < b ? a : b;
-// Thank you, Freya, for good modulo: https://github.com/FreyaHolmer/Mathfs/blob/e70cadeb2e1dcffa0ba39168e6d0be037f7e55f6/Mathfs.cs#L594-L595
+// Thank you, Freya :D https://github.com/FreyaHolmer/Mathfs/blob/e70cadeb2e1dcffa0ba39168e6d0be037f7e55f6/Mathfs.cs#L594-L595
 const mod = (v, d) => v < 0 ? (v%d+d)%d : v%d;
 
 const rand = (a=1, b=0) => a+Math.random()*(b-a);
@@ -26,4 +26,17 @@ const shuffle = arr => {
         ri = 0|rand(i);
     }
     return arr;
+};
+// Thanks, bryc :) https://stackoverflow.com/a/52171480/7429566
+const hash = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed,
+      h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
