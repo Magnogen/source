da9241935a6ebf896be8afbca93d5754b284104d
Magnogen <hydronesium@gmail.com>
2022-12-24 16:48:28 +0000

    Chance, redundant brackets and reduce lines

--- random.js
+++ random.js
@@ -1,5 +1,6 @@
 const rand = (a=1, b=0) => a+Math.random()*(b-a);
 const randpom = (a=1, b=-a) => rand(a, b);
+const chance = (prob) => rand() < prob,
 const choose = (arr) => arr[0|(rand(arr.length))];
 const shuffle = (arr) => {
     let i = arr.length, j = 0|rand(i);
@@ -18,7 +19,7 @@ const Mulberry = (seed) => {
         let t = value + 0x6D2B79F5;
         t = Math.imul(t ^ t >>> 15, t | 1);
         t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-        return ((t ^ t >>> 14) >>> 0);
+        return (t ^ t >>> 14) >>> 0;
     };
     const rand = (a=1, b=0) => a+(state=hash(state))/0x100000000*(b-a);
     const shuffle = (arr) => {
@@ -30,25 +31,23 @@ const Mulberry = (seed) => {
         return arr;
     };
     const dimension_primes = ((digits) => {
-        const max = 0|(10**digits);
+        const max = 0|(10**digits), sqrt = 0|(max**0.5);
         let numbers = [...Array(max)].map((e,i) => i-1?i:0);
-        const sqrt = 0|(max**0.5);
         const ij = [...Array(max*sqrt)]
             .map((e, i) => ({ i: i%sqrt, j: 0|(i/sqrt) }))
-            .filter(({ i, j }) => (i > 1 && j > 1));
+            .filter(({ i, j }) => i > 1 && j > 1);
         for (let { i, j } of ij) numbers[i * j] = 0;
         return shuffle(numbers.filter(n => n && n > 10**(digits-1)));
     })(3.75);
     const hash_n = (...values) => {
         let value = seed;
-        for (let i in values) {
-            value ^= dimension_primes[i]*values[i];
-        }
+        for (let v in values) value ^= dimension_primes[v]*values[v];
         return hash(value)/0x100000000;
     }
     return {
         seed, rand, hash: hash_n,
         randpom: (a=1, b=-a) => rand(a, b),
+        chance: (prob) => rand() < prob,
         choose: (arr) => arr[0|rand(arr.length)],
         shuffle
     }
