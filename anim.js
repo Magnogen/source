b383a2e63bf3b9a586e3af231514a5408580e076
Magnogen <hydronesium@gmail.com>
2023-02-11 16:01:46 +0000

    pause anim

--- anim.js
+++ anim.js
@@ -1,1 +1,5 @@
 const frame = () => new Promise(requestAnimationFrame);
+const pause = (() => {
+    let last = performance.now();
+    return (time=1000/60) => (performance.now() - last > time) ? (last = performance.now(), frame()) : undefined;
+})();
