e23f403eb0b22414521410ac9b29370a295b817c
Magnogen <hydronesium@gmail.com>
2023-03-10 14:15:35 +0000

    fix anim and add functionality

--- anim.js
+++ anim.js
@@ -1,5 +1,19 @@
-const frame = () => new Promise(requestAnimationFrame);
+const frame = (frames=1) => new Promise(async (res) => {
+    for (let i = 0; i < frames; i++) {
+        await new Promise(requestAnimationFrame);
+    }
+    res();
+});
 const pause = (() => {
     let last = performance.now();
-    return (time=1000/60) => (performance.now() - last > time) ? (last = performance.now(), frame()) : undefined;
+    return (time=1000/60, onFrame=()=>{}) => new Promise(async (res) => {
+      if (performance.now() - last > time) {
+        const result = onFrame();
+        await frame();
+        last = performance.now();
+        res(result);
+        return;
+      }
+      res(undefined);
+    })
 })();
