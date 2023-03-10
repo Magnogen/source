44d28c981245d63b31b7a80fd954b0ef43b0aa2f
Magnogen <hydronesium@gmail.com>
2023-03-10 14:17:32 +0000

    add rotation and reorganisation to vec

--- vec.js
+++ vec.js
@@ -7,13 +7,23 @@ const Vec = (() => {
     function div(val)   { this.x /= val; this.y /= val; return this }
     function length()   { return Math.hypot(this.x, this.y) }
     function angle()    { return Math.atan2(this.y, this.x) }
+    function rotate(a)  { 
+      const { x, y } = this;
+      this.x = x*Math.cos(a) - y*Math.sin(a);
+      this.y = x*Math.sin(a) + y*Math.cos(a);
+      return this;
+    }
     function dot(other) { return this.x*other.x + this.y*other.y }
-    return (x, y) => ({
-        x, y,
-        set, copy,
-        add, sub,
-        mul, div,
-        length, angle,
-        dot,
-    })
+    return (x, y) => {
+        const vector = {
+            x, y,
+            set, copy, clone: copy,
+            add, sub,
+            mul, div,
+            length,
+            angle, rotate,
+            dot,
+        };
+        return vector
+    }
 })();
