40372041a53325ec3db3617482778e6884a17b0a
Magnogen <hydronesium@gmail.com>
2023-06-22 21:59:37 +0100

    Update vec.js to vec2

--- vec.js
+++ vec.js
@@ -1,29 +1,25 @@
-const Vec = (() => {
-    function set(x, y)  { [this.x, this.y] = [x, y]; return this }
-    function copy()     { return Vec(this.x, this.y) }
-    function add(other) { this.x += other.x; this.y += other.y; return this }
-    function sub(other) { this.x -= other.x; this.y -= other.y; return this }
-    function mul(val)   { this.x *= val; this.y *= val; return this }
-    function div(val)   { this.x /= val; this.y /= val; return this }
-    function length()   { return Math.hypot(this.x, this.y) }
-    function angle()    { return Math.atan2(this.y, this.x) }
-    function rotate(a)  { 
-      const { x, y } = this;
-      this.x = x*Math.cos(a) - y*Math.sin(a);
-      this.y = x*Math.sin(a) + y*Math.cos(a);
-      return this;
+const vec2 = (() => {
+    function add(other)  { return vec2(this.x + other.x, this.y + other.y) }
+    function sub(other)  { return vec2(this.x - other.x, this.y - other.y) }
+    function mul(val)    { return vec2(this.x * val, this.y * val) }
+    function div(val)    { return vec2(this.x / val, this.y / val) }
+    function length()    { return Math.hypot(this.x, this.y) }
+    function angle()     { return Math.atan2(this.y, this.x) }
+    function turn(a)     { return vec2(
+        this.x*Math.cos(a) - this.y*Math.sin(a),
+        this.x*Math.sin(a) + this.y*Math.cos(a)
+    ) }
+    function dot(other)  { return this.x*other.x + this.y*other.y }
+    function normalize() {
+        const len = this.length();
+        return vec2(this.x / len, this.y / len);
     }
-    function dot(other) { return this.x*other.x + this.y*other.y }
-    return (x, y) => {
-        const vector = {
-            x, y,
-            set, copy, clone: copy,
-            add, sub,
-            mul, div,
-            length,
-            angle, rotate,
-            dot,
-        };
-        return vector
-    }
+    return (x, y) => ({
+        x, y,
+        add, sub,
+        mul, div,
+        length, normalize,
+        angle, turn,
+        dot,
+    })
 })();
