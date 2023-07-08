c8ad200e27d276dfcdf12fe591c1d9182c0d8122
Magnogen <hydronesium@gmail.com>
2023-07-08 16:35:07 +0100

    more functionality to shader.js

--- shader.js
+++ shader.js
@@ -1,24 +1,29 @@
-const Shader = (canvas, N) => {
+const Shader = async (canvas, N, libs=[]) => {
   const context = canvas.getContext('2d');
 
   const workerCode = (()=>{
+    
+    {{LIBRARIES}}
+    
     let shadePixel;
-    onmessage = (event) => {
-      if (event.data.type == 'init') {
-        let { funcString, width, height } = event.data;
-        shadePixel = new Function('return ' + funcString)()(width, height);
-        postMessage(true)
+
+    onmessage = async (event) => {
+      if (event.data.type === 'init') {
+        let { funcString, width, height, params } = event.data;
+        shadePixel = await new Function('return ' + funcString)()(width, height, params);
+        postMessage(true);
         return;
       }
-      let { imageData, xStart, yStart } = event.data;
 
+      let { imageData, xStart, yStart } = event.data;
       const data = imageData.data;
       for (let i = 0; i < data.length; i += 4) {
         const x = xStart + (i / 4) % imageData.width;
         const y = yStart + Math.floor(i / 4 / imageData.width);
         const pixel = { r: data[i], g: data[i + 1], b: data[i + 2], a: data[i + 3] };
         const newPixel = shadePixel({ x, y });
-        data[i + 0] = newPixel.r;
+
+        data[i] = newPixel.r;
         data[i + 1] = newPixel.g;
         data[i + 2] = newPixel.b;
         data[i + 3] = newPixel.a;
@@ -26,21 +31,32 @@ const Shader = (canvas, N) => {
 
       postMessage(imageData);
     };
-  }).toString().slice(6, -4).split('\n').map(e=>e.slice(4)).join('\n');
+  }).toString()
+  .slice(6, -4)
+  .split('\n')
+  .map(e => e.slice(4))
+  .join('\n')
+  .replace(
+    '{{LIBRARIES}}',
+    (await Promise.all(
+      libs.map(url =>
+        fetch(url)
+        .then(e=>e.text())
+      )
+    )).join('\n\n')
+  );
 
   const splitImage = (ctx, N) => {
     const { canvas } = ctx;
     const { width, height } = canvas;
-    
     const sections = [];
-    
+
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < N; j++) {
-        const xStart = j * (width/N);
-        const yStart = i * (height/N);
-        const squareWidth = Math.min(width/N, width - xStart);
-        const squareHeight = Math.min(height/N, height - yStart);
-
+        const xStart = j * Math.ceil(width / N);
+        const yStart = i * Math.ceil(height / N);
+        const squareWidth = Math.min(Math.ceil(width / N), width - xStart);
+        const squareHeight = Math.min(Math.ceil(height / N), height - yStart);
         const squareImageData = ctx.getImageData(xStart, yStart, squareWidth, squareHeight);
 
         sections.push({ imageData: squareImageData, xStart, yStart });
@@ -50,8 +66,7 @@ const Shader = (canvas, N) => {
     return sections;
   };
 
-  const shade = (func) => {
-    
+  const shade = (func, params = {}) => {
     let taskQueue = [];
     const workers = [...Array(navigator.hardwareConcurrency)].map(e => {
       const blob = new Blob([workerCode], { type: "text/javascript" });
@@ -59,6 +74,7 @@ const Shader = (canvas, N) => {
       const worker = new Worker(workerUrl);
       let busy = false;
       let res;
+
       worker.onmessage = (event) => {
         res({ worker, imageData: event.data });
         if (taskQueue.length < 1) return;
@@ -66,36 +82,47 @@ const Shader = (canvas, N) => {
         res = task.res;
         worker.postMessage(task.data);
       };
+
       return {
         init() {
-          return new Promise((resolve, rej) => {
+          return new Promise((resolve, reject) => {
             res = resolve;
             worker.postMessage({
               type: 'init',
               funcString: func.toString(),
               width: canvas.width,
-              height: canvas.height
+              height: canvas.height,
+              params
             });
-          })
+          });
         }
-      }
+      };
     });
-    
-    const queue = (data) => new Promise((res, rej) => { taskQueue.push({ res, data }) });
-    
-    const sections = splitImage(context, N);
 
-    return new Promise(async (resolve, reject) => {
-      for (let { imageData, xStart, yStart } of sections) {
+    const queue = (data) => new Promise((resolve, reject) => {
+      taskQueue.push({ res: resolve, data });
+    });
+
+    const sections = splitImage(context, N);
+    let promises = [];
+
+    for (let { imageData, xStart, yStart } of sections) {
+      promises.push(
         queue({
           imageData,
-          xStart, yStart,
+          xStart,
+          yStart,
         }).then((result) => {
           context.putImageData(result.imageData, xStart, yStart);
         })
-      }
-      for (const worker of workers) worker.init();
-    });
+      );
+    }
+
+    for (const worker of workers) {
+      worker.init();
+    }
+
+    return Promise.all(promises);
   };
 
   return {
