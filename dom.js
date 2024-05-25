eaff1f722b587a9dc9ea7c6c559917b2c48a3000
Magnogen <hydronesium@gmail.com>
2024-05-25 21:52:39 +0100

    dom make

--- dom.js
+++ dom.js
@@ -1,5 +1,15 @@
 const $ = (query) => document.querySelector(query);
 const $$ = (query) => document.querySelectorAll(query);
+// thanks lski and yne :)
+// https://gist.github.com/lski/7de19ff1b6a460c47190fbed487c5f19
+const $make = (type, attributes={}, children=[]) => {
+    let el = Object.assign(document.createElement(type), attributes);
+    for (const child of children) {
+        if (typeof child == 'string') el.appendChild(document.createTextNode(child));
+        else el.appendChild(child);
+    }
+    return el;
+};
 EventTarget.prototype.on = function (...args) {
     return this.addEventListener(...args);
 };
