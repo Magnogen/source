4fc321b336d1057511c1bfb5b0f2e51ad957e751
Magnogen <hydronesium@gmail.com>
2025-05-25 16:53:44 +0100

    add El to dom

--- dom.js
+++ dom.js
@@ -8,3 +8,19 @@ EventTarget.prototype.trigger = function (name, options={}) {
     for (const key in options) event[key] = options[key];
     return this.dispatchEvent(event)
 };
+
+const El = (tag) => (...children) => {
+    const el = document.createElement(tag);
+    for (const child of children) {
+        el.appendChild(
+            typeof child === "string" ?
+                document.createTextNode(child)
+            : child instanceof Node ?
+                child
+            :
+                null
+        );
+    }
+    return el;
+};
+const tags = new Proxy({}, { get: (, tag) => El(tag) });
